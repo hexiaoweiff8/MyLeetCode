1a0a0b7e6258674c6c5d80311927555733dfc488
hxw <hexiaoweiff8@sina.com>
2025-04-17 13:52:46 +0800

    每日一题2176

---
+++ code/NC2176.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def countPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(nums)
+        ans = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] == nums[j] and (i * j) % k == 0:
+                    ans += 1
+
+        return ans
+
+
+obj = Solution()
+print(obj.countPairs([3, 1, 2, 2, 2, 1, 3], 2))
