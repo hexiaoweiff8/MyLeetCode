de8752633b0072aea729c91b7635d61d2b6afbab
hxw <hexiaoweiff8@sina.com>
2025-06-18 08:53:46 +0800

    每日一题2966

---
+++ code/2966.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def divideArray(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        ans = []
+        for i in range(0, len(nums), 3):
+            # 如三个值中任意两个值差值超过k, 则返回空数组
+            if abs(nums[i] - nums[i + 1]) > k\
+                    or abs(nums[i] - nums[i + 2]) > k\
+                    or abs(nums[i + 1] - nums[i + 2]) > k:
+                return []
+            ans.append(nums[i:i+3])
+
+        return ans
