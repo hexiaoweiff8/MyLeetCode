8e6d5a897d1054790da33f609f67f2c6e04d67bb
hxw <hexiaoweiff8@sina.com>
2025-10-14 10:02:22 +0800

    每日一题3349

---
+++ code/NC3349.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def hasIncreasingSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        n = len(nums)
+        ans = pre_cnt = cnt = 0
+        for i, x in enumerate(nums):
+            cnt += 1
+            if i == n - 1 or x >= nums[i + 1]:
+                ans = max(ans, cnt // 2, min(cnt, pre_cnt))
+                pre_cnt = cnt
+                cnt = 0
+
+        return ans >= k
+
+
+obj = Solution()
+print(obj.hasIncreasingSubarrays([16, 16], 1))
