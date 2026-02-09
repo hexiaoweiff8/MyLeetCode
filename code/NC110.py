5c9406393f066a3b8aca1b3eaec129d74b332ed5
hxw <hexiaoweiff8@sina.com>
2026-02-09 13:29:23 +0800

    每日一题110

---
+++ code/NC110.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        def dfs(root):
+            if not root:
+                return 0
+            left = dfs(root.left)
+            if left == -1:
+                return -1
+            right = dfs(root.right)
+            if right == -1:
+                return -1
+            if abs(left - right) > 1:
+                return -1
+            return max(left, right) + 1
+
+        return dfs(root) != -1
+
