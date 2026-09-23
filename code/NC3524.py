458f23efa0e828dc4ac4a3ae2493d80d2875e8fd
hxw <hexiaoweiff8@sina.com>
2026-09-23 09:41:37 +0800

    每日一题NC3524

---
+++ code/NC3524.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def minOperations(self, nums, x):
+        """
+        :type nums: List[int]
+        :type x: int
+        :rtype: int
+        """
+        # 左往右滑动匹配
+        lenNums = len(nums)
+        leftIndex = 0
+        rightIndex = 0
+        leftSum = 0
+        rightSum = sum(nums)
+        minCount = lenNums + 1
+        if rightSum == x:
+            return lenNums
+        if rightSum < x:
+            return -1
+        for leftIndex in range(-1, lenNums - 1):
+            if leftIndex > -1:
+                leftSum += nums[leftIndex]
+            while leftSum + rightSum > x and rightIndex < lenNums:
+                rightSum -= nums[rightIndex]
+                rightIndex += 1
+            if leftSum + rightSum == x:
+                minCount = min(minCount, leftIndex + 1 + lenNums - rightIndex)
+
+        return -1 if minCount > lenNums else minCount
